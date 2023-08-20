b26b6eba61f4b8dee1f14c126d036b94ed3d1a9b
Nitin Kumar <nitin.l.kumar@oracle.com>
2023-08-20 15:25:08 +0530

    update config for mac system

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 # List of directories
-music_dirs = [r"C:\Users\nitin\Music\Tracks",r"C:\Users\nitin\Music\The great Ones",
-r"C:\Users\nitin\Music\The great Ones\Sidetracked"]
-
+music_dirs = [r"/Users/kuknitin/Music"]
 player_path = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
-# C:\Users\nitin\Music\Tracks\Alone.mp3
-# sb.run([r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe", r"C:\Users\nitin\Music\Tracks\Alone.mp3"])
-
 
 dm = True
 path_seperator = "\\" 
